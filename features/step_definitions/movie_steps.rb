95319707595f913be012b591f6c93d08b41b486e
Mehran Halimi <mehran.net1@gmail.com>
2012-08-12 03:14:56 +0800

    A1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,9 +4,37 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    #movie[:title] = ['Aladdin', 'The Terminator', 'When Harry Met Sally', 'The Help', 'Chocolat', 'Amelie', '2001: A Space Odyssey', 'The Incredibles', 'Raiders of the Lost Ark', 'Chicken Run ']
+    #movie[:rating] = ['G', 'R', 'R', 'PG-13', 'PG-13', 'R', 'G', 'PG', 'PG', 'G']
+    #movie[:release_date] = ['25-Nov-1992', '26-Oct-1984', '21-Jul-1989', '10-Aug-2011', '5-Jan-2001', '25-Apr-2001', '6-Apr-1968', '5-Nov-2004', '12-Jun-1981', '21-Jun-2000']
+    movie.create({:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'});
+    movie.create({:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'});
+    movie.create({:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'});
+    movie.create({:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'});
+    movie.create({:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'});
+    movie.create({:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'});
+    movie.create({:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'});
+    movie.create({:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'});
+    movie.create({:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'});
+    movie.create({:title => 'Chicken Run ', :rating => 'G', :release_date => '21-Jun-2000'});
+	# each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+	Given %Q{I am on the Create New Movie page}
+	When  %Q{I fill in "Title" with "#{movie[:title]}"}
+	And   %Q{I select "#{movie[:rating]}" from "Rating"}
+	And   %Q{I select "#{movie[:release_date]}" from "Released On"}
+	And   %Q{I press "Save Changes"}
   end
-  flunk "Unimplemented"
+  assert false, "Unimplmemented"
 end
+
+#Given /the following movies exist/ do |movies_table|
+#  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+#  end
+#  flunk "Unimplemented"
+#end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
