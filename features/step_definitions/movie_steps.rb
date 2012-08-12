59ab8dbbaa606857662d24d837e316eb23e5ad97
Mehran Halimi <mehran.net1@gmail.com>
2012-08-12 14:18:18 +0800

    B1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -27,12 +27,11 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
 	  # HINT: use String#split to split up the rating_list, then
 	  #   iterate over the ratings and reuse the "When I check..." or
 	  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-	  rating_list|
 	  rating_list.split(",").each do |field|
 		field = field.strip
 		if uncheck == "un"
-		   step %Q{I uncheck "ratings_#{field}"}
-		   step %Q{the "ratings_#{field}" checkbox should not be checked}
+		  step %Q{I uncheck "ratings_#{field}"}
+		  step %Q{the "ratings_#{field}" checkbox should not be checked}
 		else
 		  step %Q{I check "ratings_#{field}"}
 		  step %Q{the "ratings_#{field}" checkbox should be checked}
